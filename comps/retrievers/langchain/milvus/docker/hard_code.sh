529a3006c3957f40b986d2bfd2141ca68f615114
letonghan <letong.han@intel.com>
2024-07-12 15:12:29 +0800

    update retriever model
    Signed-off-by: letonghan <letong.han@intel.com>

---
+++ comps/retrievers/langchain/milvus/docker/hard_code.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE="/home/user/.local/lib/python3.11/site-packages/langchain_milvus/vectorstores/milvus.py"
+
+MARKER='if index is not None:'
+
+TEMP_FILE=$(mktemp)
+
+cat <<EOL > "$TEMP_FILE"
+                index["index_param"] = self.index_params
+EOL
+
+sed -i "/$MARKER/r $TEMP_FILE" "$FILE"
+
+rm "$TEMP_FILE"
